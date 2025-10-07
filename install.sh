e8db02bf5692bd92ebd18ad34882dd1e6e814234
MaximSaka <88205887+MaximSaka@users.noreply.github.com>
2025-10-06 17:11:44 -0700

    Revert back changes incompatible with QLI 1.5

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 BASE_DIR=""
 DEMO_APP_DIR=$BASE_DIR"/opt/QCS6490-Vision-AI-Demo"
+outputmodelpath="/etc/models"
+outputlabelpath="/etc/labels"
 
 echo "Make file system writable"
 mount -o remount,rw /
@@ -47,27 +49,18 @@ download_file() {
 }
 
 
-outputmodelpath="/etc/models"
-outputlabelpath="/etc/labels"
-
 mkdir -p "${outputmodelpath}"
 mkdir -p "${outputlabelpath}"
 
-### Labels
-download_file "https://raw.githubusercontent.com/quic/sample-apps-for-qualcomm-linux/refs/heads/main/artifacts/json_labels/hrnet_pose.json" "${outputlabelpath}/hrnet_pose.json"
-download_file "https://raw.githubusercontent.com/quic/sample-apps-for-qualcomm-linux/refs/heads/main/artifacts/json_labels/classification.json" "${outputlabelpath}/classification.json"
-download_file "https://raw.githubusercontent.com/quic/sample-apps-for-qualcomm-linux/refs/heads/main/artifacts/json_labels/deeplabv3_resnet50.json" "${outputlabelpath}/deeplabv3_resnet50.json"
-download_file "https://raw.githubusercontent.com/quic/sample-apps-for-qualcomm-linux/refs/heads/main/artifacts/json_labels/yolox.json" "${outputlabelpath}/yolox.json"
-download_file "https://raw.githubusercontent.com/quic/sample-apps-for-qualcomm-linux/refs/heads/main/artifacts/json_labels/monodepth.json" "${outputlabelpath}/monodepth.json"
 
+download_file "https://raw.githubusercontent.com/quic/sample-apps-for-qualcomm-linux/refs/heads/main/artifacts/labels/hrnet_pose.labels" "${outputlabelpath}/hrnet_pose.labels"
+download_file "https://raw.githubusercontent.com/quic/sample-apps-for-qualcomm-linux/refs/heads/main/artifacts/labels/classification.labels" "${outputlabelpath}/classification.labels"
+download_file "https://raw.githubusercontent.com/quic/sample-apps-for-qualcomm-linux/refs/heads/main/artifacts/labels/deeplabv3_resnet50.labels" "${outputlabelpath}/voc_segmentation.labels"
+download_file "https://raw.githubusercontent.com/quic/sample-apps-for-qualcomm-linux/refs/heads/main/artifacts/labels/detection.labels" "${outputlabelpath}/yolox.labels"
+download_file "https://qaihub-public-assets.s3.us-west-2.amazonaws.com/qai-hub-models/models/midas/midasv2_linux_assets/monodepth.labels" "${outputlabelpath}/monodepth.labels"
 
-### Model files
 download_file "https://huggingface.co/qualcomm/Inception-v3/resolve/60c6a08f58919a0dc7e005ec02bdc058abb1181b/Inception-v3_w8a8.tflite" "${outputmodelpath}/inception_v3_quantized.tflite"
-download_file "https://huggingface.co/qualcomm/DeepLabV3-Plus-MobileNet/resolve/fa276f89cce8ed000143d40e8887decbbea57012/DeepLabV3-Plus-MobileNet_w8a8.tflite" "${outputmodelpath}/deeplabv3_plus_mobilenet_quantized.tflite"
+download_file "https://huggingface.co/qualcomm/FCN-ResNet50/resolve/3951b964f2b231d28ca0d04091dc661b0cc3f53c/FCN-ResNet50_w8a8.tflite" "${outputmodelpath}/fcn_resnet50_quantized.tflite"
 download_file "https://huggingface.co/qualcomm/HRNetPose/resolve/dbfe1866bd2dbfb9eecb32e54b8fcdc23d77098b/HRNetPose_w8a8.tflite" "${outputmodelpath}/hrnet_pose_quantized.tflite"
 download_file "https://huggingface.co/qualcomm/Midas-V2/resolve/13de42934d09fe7cda62d7841a218cbb323e7f7e/Midas-V2_w8a8.tflite" "${outputmodelpath}/midas_quantized.tflite"
 download_file "https://huggingface.co/qualcomm/Yolo-X/resolve/2885648dda847885e6fd936324856b519d239ee1/Yolo-X_w8a8.tflite" "${outputmodelpath}/yolox_quantized.tflite"
-
-
-### Settings
-download_file "https://raw.githubusercontent.com/quic/sample-apps-for-qualcomm-linux/refs/heads/main/artifacts/json_labels/hrnet_settings.json" "${outputlabelpath}/hrnet_settings.json"
